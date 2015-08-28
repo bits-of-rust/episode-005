b7f32273b8b0cb1979e144058d27cb2d1fa1c912
dvberkel <daan.v.berkel.1980@gmail.com>
2015-08-28 21:05:11 +0200

    Save code after episode is recorded

--- immutabillity/src/main.rs
+++ immutabillity/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-	let subject = "world";
+	let mut subject = "world";
 	
+    println!("Hello, {}!", subject);
+    println!("Goodbye, {}!", subject);
+
+    subject = "moon";
+
     println!("Hello, {}!", subject);
     println!("Goodbye, {}!", subject);
 }
